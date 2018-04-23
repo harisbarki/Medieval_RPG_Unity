9759e11d0e1d30fa43541ec0a8eefc6fe0f49d44
Haris Barki <harisbarki@ymail.com>
2018-04-23 18:43:36 -0230

    reorganized code and added click movement and WASD movement change on G btn

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,90 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float walkMoveStopRadius = 0.2f;
+
+    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+    Boolean mouseClicked;
+
+    bool isInDirectMode = false;    // TODO consider making static later
+
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // TODO fix issue with click to move and WSAD conflicting and increasing speed
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        // TODO allow player to map later or add to menu
+        if (Input.GetKeyDown(KeyCode.G))    // G for gamepad. 
+        {
+            isInDirectMode = !isInDirectMode;   // Toggle mode
+        }
+
+        if (isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+        m_Character.Move(m_Move, false, false);
+
+    }
+
+    private void ProcessMouseMovement()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            switch (cameraRaycaster.layerHit)
+            {
+                case Layer.Walkable:
+                    currentClickTarget = cameraRaycaster.hit.point;
+                    break;
+                case Layer.Enemy:
+                    print("Not moving to Enemy");
+                    break;
+                default:
+                    print("Unexpected layer found!");
+                    return;
+            }
+        }
+
+        var playerToClickPoint = currentClickTarget - transform.position;
+
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius)
+        {
+            m_Character.Move(playerToClickPoint, false, false);
+        }
+        else
+        {
+            m_Character.Move(Vector3.zero, false, false);
+        }
+    }
+}
+
