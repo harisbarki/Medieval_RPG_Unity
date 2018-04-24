a8acf9df68567d19f28cb7f5bf437cb36d96cb5b
Haris Barki <harisbarki@ymail.com>
2018-04-24 14:48:19 -0230

    added windmill rotation code and pathfinding for enemies using nav mesh

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour
 {
 
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float attackRadius = 4f;
+
     float currentHealthPoints = 100f;
+    AICharacterControl aICharacterControl = null;
+    GameObject player = null;
 
     public float healthAsPercentage
     {
@@ -19,12 +24,21 @@ public class Enemy : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player");
+        aICharacterControl = GetComponent<AICharacterControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer <= attackRadius)
+        {
+            aICharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aICharacterControl.SetTarget(transform);
+        }
     }
 }
