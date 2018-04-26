9f5ecff695bb55faa691b921b62812ab08c49c30
Haris Barki <harisbarki@ymail.com>
2018-04-25 23:55:33 -0230

    refractored

---
+++ Assets/Weapons/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public float projectileSpeed = 10f;
+
+    float damageCaused = 10f;
+
+    public void SetDamage(float damage)
+    {
+        damageCaused = damage;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Component damageableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
+        if (damageableComponent)
+        {
+            (damageableComponent as IDamageable).TakeDamage(damageCaused);
+        }
+
+        // clean up
+        Destroy(gameObject, 0.01f);
+    }
+}
