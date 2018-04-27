aeb2e3a053bda9761738f97ae3d95c30b19ccc75
Haris Barki <harisbarki@ymail.com>
2018-04-26 22:04:22 -0230

    added attack animation

--- Assets/Weapons/Projectile.cs
+++ Assets/Weapons/Projectile.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
-    public float projectileSpeed; // Note other classes can set
-
+    [SerializeField] float projectileSpeed;
+    GameObject shooter;
     float damageCaused;
+    const float DESTORY_DELAY = 0.01f;
+
+    public float GetDefaultLaunchSpeed()
+    {
+        return projectileSpeed;
+    }
 
     public void SetDamage(float damage)
     {
         damageCaused = damage;
     }
 
+    public void SetShooter(GameObject shooter)
+    {
+        this.shooter = shooter;
+    }
+
     void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer != shooter.layer)
+        {
+            DamageIfDamageables(collision);
+        }
+    }
+
+    private void DamageIfDamageables(Collision collision)
     {
         Component damagableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
         if (damagableComponent)
         {
             (damagableComponent as IDamageable).TakeDamage(damageCaused);
         }
-        Destroy(gameObject, 0.01f);
+        Destroy(gameObject, DESTORY_DELAY);
     }
 }
