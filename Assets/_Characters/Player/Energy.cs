28bb1737523e33cd2705cc1c784b3187302d508d
Haris Barki <harisbarki@ymail.com>
2018-04-28 12:55:15 -0230

    added energy bar

---
+++ Assets/_Characters/Player/Energy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using RPG.CameraUI;
+
+namespace RPG.Characters
+{
+    public class Energy : MonoBehaviour
+    {
+
+        [SerializeField] RawImage energyBar;
+        [SerializeField] float maxEnergyPoints = 100f;
+        [SerializeField] float pointsPerHit = 10f;
+
+        CameraRaycaster cameraRaycaster = null;
+        float currentEnergyPoints = 100f;
+
+        // Use this for initialization
+        void Start()
+        {
+            currentEnergyPoints = maxEnergyPoints;
+            cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+            cameraRaycaster.notifyRightClickObservers += ProcessRightClickMovement;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void ProcessRightClickMovement(RaycastHit raycastHit, int layer)
+        {
+            currentEnergyPoints = Mathf.Clamp(currentEnergyPoints - pointsPerHit, 0, maxEnergyPoints);
+            UpdateEnergyBar();
+        }
+
+        private void UpdateEnergyBar()
+        {
+            float xValue = -(EnergyAsPercent() / 2f) - 0.5f;
+            energyBar.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+        }
+
+        float EnergyAsPercent()
+        {
+            return currentEnergyPoints / maxEnergyPoints;
+        }
+    }
+}
