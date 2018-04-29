af649f11b1253ff3afd891131a647dd75c3a8c91
Haris Barki <harisbarki@ymail.com>
2018-04-29 02:16:46 -0230

    Weapon pickup added

---
+++ Assets/_Weapons/WeaponPickupPoint.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RPG.Characters;
+
+namespace RPG.Weapons
+{
+    [ExecuteInEditMode]
+    public class WeaponPickupPoint : MonoBehaviour
+    {
+
+        [SerializeField] Weapon weaponConfig;
+        [SerializeField] AudioClip pickupSFX;
+
+        AudioSource audioSource;
+
+        // Use this for initialization
+        void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(!Application.isPlaying)
+            {
+                DestroyChildren();
+                InstantiateWeapon();
+            }
+        }
+
+        void DestroyChildren()
+        {
+            foreach (Transform child in transform)
+            {
+                DestroyImmediate(child.gameObject);
+            }
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            audioSource.PlayOneShot(pickupSFX);
+            FindObjectOfType<Player>().PutWeaponInHand(weaponConfig);
+        }
+
+        void InstantiateWeapon()
+        {
+            var weapon = weaponConfig.GetWeaponPrefab();
+            weapon.transform.position = Vector3.zero;
+            Instantiate(weapon, gameObject.transform);
+        }
+    }
+}
